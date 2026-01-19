cb6dba10d63033f22ae0900f0662b60bbdd5a843
woshishuaige2 <bgong22@wisc.edu>
2026-01-19 11:36:42 -0500

    new offline video

--- Code/IMU/main.py
+++ Code/IMU/main.py
@@ -4,3 +4,5 @@ from app.app import main
 
 if __name__ == "__main__":
     main()
+
+# Run offline video: d C:\Users\china\OneDrive\Documents\awesome-dodeca-pen\Code\IMU; C:/Users/china/OneDrive/Documents/awesome-dodeca-pen/.venv/Scripts/python.exe main.py --mode cv_only --video ..\Computer_vision\src\offline_test.mp4
